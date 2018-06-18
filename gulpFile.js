c78ebc164644db64409880e1bd69fc5ced7bf4ee
cicomsa <ioanacomsa85@gmail.com>
2018-06-18 23:45:35 +0100

    copy html

--- gulpFile.js
+++ gulpFile.js
@@ -1,38 +1,43 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const browserSync = require('browser-sync')
-const reload = browserSync.reload
-const autoprefixer = require('gulp-autoprefixer')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
 
-const sourcePath = {
-  sassSource : "src/scss/*.scss"
+const sourcePaths = {
+  sassSource : 'src/scss/*.scss',
+  htmlSource : 'src/*.html'
 }
-
-const appPath = {
+const appPath ={
   root: 'app/',
   css : 'app/css',
-  js : 'app.js'
+  js : 'app/js'
 }
 
-gulp.task('sass', () => {
-  return gulp.src(sourcePath.sassSource)
-    .pipe(autoprefixer())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest(appPath.css));
-})
 
+gulp.task('sass', function(){
+  return gulp.src(sourcePaths.sassSource)
+      .pipe(autoprefixer())
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(gulp.dest(appPath.css));
+});
 
-gulp.task('serve', ['sass'], () => {
+gulp.task('copy', function() {
+  gulp.src(sourcePaths.htmlSource)
+      .pipe(gulp.dest(appPath.root))
+});
+
+gulp.task('serve', ['sass'], function() {
   browserSync.init([appPath.css + '/*.css', appPath.root + '/*.html', appPath.js + '/*.js'], {
     server: {
       baseDir : appPath.root
     }
   })
-})
+});
 
-gulp.task('watch', ['serve', 'sass'], () => {
-  gulp.watch([sourcePath.sassSource], ['sass']);
-})
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
+  gulp.watch([sourcePaths.sassSource], ['sass']);
+  gulp.watch([sourcePaths.htmlSource], ['copy']);
+} );
 
-
-gulp.task('default', ['watch'])
+gulp.task('default', ['watch']);

e6d82f43dcfaee1a788e8ae418e5471d714f7461
cicomsa <ioanacomsa85@gmail.com>
2018-06-18 22:31:57 +0100

    browserSync and gulp watch added

--- gulpFile.js
+++ gulpFile.js
@@ -1,10 +1,36 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+
+const sourcePath = {
+  sassSource : "src/scss/*.scss"
+}
+
+const appPath = {
+  root: 'app/',
+  css : 'app/css',
+  js : 'app.js'
+}
 
 gulp.task('sass', () => {
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(sourcePath.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest(appPath.css))
 })
 
-gulp.task('default', ['sass'])
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init([appPath.css + '/*.css', appPath.root + '/*.html', appPath.js + '/*.js'], {
+    server: {
+      baseDir : appPath.root
+    }
+  })
+})
+
+gulp.task('watch', ['serve', 'sass'], () => {
+  gulp.watch([sourcePath.sassSource], ['sass']);
+})
+
+
+gulp.task('default', ['watch'])

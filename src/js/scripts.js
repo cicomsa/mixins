76f81e7c53543dc66f1d568b0d7a633ae7ac7ee4
cicomsa <ioanacomsa85@gmail.com>
2018-06-19 08:04:50 +0100

    jquery, boostrap-only chrome working well, mustache added

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+global.jQuery = require('jquery');
+bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+jQuery(document).ready(function() {
+    alert("hello");
+});
+
+jQuery(document).ready(function() {
+    alert("me");
+});
